1fd284e7cc7e1e1b5e63e5f47cc7eefd19256e0c
Sebastian Crossa <crossasebastian@gmail.com>
2019-03-10 19:02:46 -0600

    Clients can now be added onto the firestore DB through the app

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -12,8 +12,27 @@ import { firestore } from 'firebase';
 import Spinner from '../layout/Spinner'
 
 class Clients extends Component {
+    state = {
+        totalOwed: null
+    };
+
+    static getDerivedStateFromProps(props, state) {
+        const { clients } = props;
+
+        if (clients) {
+            const total = clients.reduce((total, client) => {
+                return total + parseFloat(client.balance.toString());
+            }, 0);
+
+            return { totalOwed: total };
+        }
+
+        return null;
+    }
+
     render() {
         const { clients } = this.props;
+        const { totalOwed } = this.state;
 
         // Check if there are any clients on the database
         if (clients) {
@@ -26,7 +45,12 @@ class Clients extends Component {
                         </div>
 
                         <div className="col-md-6">
-
+                            <h5 className="text-right text-secondary">
+                                Total owed: {' '}
+                                <span className="text-primary">
+                                    ${parseFloat(totalOwed).toFixed(2)}
+                                </span>
+                            </h5>
                         </div>
                     </div>
 
